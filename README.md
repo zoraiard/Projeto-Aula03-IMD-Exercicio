f15fe1ae5d907da73eeea702aed3fc3cf24475e2
zoraiard <zoraiarodrigues@gmail.com>
2019-05-07 23:22:18 -0300

    Criação do controller de veiculo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Projeto-Aula03-IMD-Exercicio
+
+## Aula 03 - Validação de Campos no JSF
+
---
+++ src/br/ufrn/imd/controllers/CadastrarVeiculoMBaean.java
@@ -0,0 +1,46 @@
+package br.ufrn.imd.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import br.ufrn.imd.model.Veiculo;
+
+@ManagedBean
+@SessionScoped
+public class CadastrarVeiculoMBaean {
+
+	private Veiculo veiculo;
+	private List<Veiculo> veiculos;
+	
+	public CadastrarVeiculoMBaean() {
+		this.veiculo = new Veiculo();
+		veiculos = new ArrayList();
+	}
+	
+	public String cadastrarVeiculo() {
+		veiculos.add(veiculo);
+		FacesMessage msg = new FacesMessage("Veículo Cadastrado com sucesso!");
+		msg.setSeverity(FacesMessage.SEVERITY_INFO);
+		FacesContext.getCurrentInstance().addMessage("", msg);
+		return "/form_veiculo.jsf";
+	}
+	
+	public String entrarCadastro() {
+		return "form_veiculo.jsf";
+	}
+	
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public List<Veiculo> getVeiculos() {
+		return veiculos;
+	}
+	
+	
+}
